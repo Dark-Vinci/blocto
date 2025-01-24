2ca9e8116a62844ddcf5cd8f94e2b1015c7d1b2a
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-01-24 12:57:58 +0000

    chore; more

--- src/components/If/If.tsx
+++ src/components/If/If.tsx
@@ -9,6 +9,6 @@ export function If({condition, element}: ifProps): JSX.Element {
     if (!condition) {
         return null as unknown as JSX.Element;
     }
-
+    
     return element as any as JSX.Element;
 }
